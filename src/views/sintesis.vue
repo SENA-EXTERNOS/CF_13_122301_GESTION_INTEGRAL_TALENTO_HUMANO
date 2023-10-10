c6568bdd7392a637cc3804dba82935967b0c59a0
jorozcop <orojacobo@gmail.com>
2023-10-10 18:21:30 -0500

    Ajustes Oct 10-2023

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -9,7 +9,7 @@
     .row.justify-content-center.mt-5
       .col-lg-10.mb-5
         figure
-          img(src="@/assets/curso/temas/sintesis.png", alt="alt")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
       .col-auto
         a.anexo.mb-4(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
           .anexo__icono
